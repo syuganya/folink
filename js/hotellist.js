2790488f333baac87ec46237ee9373f98eed646c
yuganya <syuganya@arvindinternet.com>
2015-07-08 23:23:31 +0530

    hotel list page created

---
+++ js/hotellist.js
@@ -0,0 +1,3 @@
+$("#ex2").slider({});
+
+$('.slider-handle').append('<div style="left: 100% ; border: 1px solid #fff; margin-top: 0px;"></div> <div style = "left: 100% ; border: 1px solid #fff; margin-top: 2px;"> </div> <div style="left:100%;border:1px solid #fff;margin-top: 2px;"> </div>');
